99d3f775f038836cd22f8516bd3e7f0623283f9b
Chris Arderne <chris@rdrn.me>
2021-12-11 11:40:30 +0200

    Balance printing

--- star.py
+++ star.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+import sys
 from pprint import pprint
 from pathlib import Path
+import datetime
 
 import httpx
 import attr
@@ -12,8 +14,17 @@ import yaml
 
 def log(it):
     print("\n\n\n")
-    pprint(it)
+    pprint(it, stream=sys.stderr)
     print()
+
+
+def parse_accs(accs):
+    if accs == "all":
+        return [p.stem for p in Path(".").glob("*.token")]
+    elif "," in accs:
+        return accs.split(",")
+    else:
+        return [accs]
 
 
 @define(init=False)
@@ -59,12 +70,25 @@ class Account:
     def headers(self):
         return {"Authorization": f"Bearer {self.token}"}
 
-    @property
-    def balance(self):
+    def print_balance(self, verbose=False):
         url = f"/api/v2/accounts/{self.uid}/balance"
         r = httpx.get(self.conf.base + url, headers=self.headers)
         data = r.json()
-        return data
+        if verbose:
+            keys = [
+                "clearedBalance",
+                "effectiveBalance",
+                "pendingTransactions",
+                "amount",
+                "totalClearedBalance",
+                "totalEffectiveBalance",
+            ]
+            for k in keys:
+                print(f"{k:<23}: {data[k]['minorUnits']/100}", f=sys.stderr)
+        date = datetime.date.today().isoformat()
+        acct = f"Assets:Starling:{self.acc.capitalize()}"
+        bal = data["totalEffectiveBalance"]["minorUnits"] / 100
+        print(f"{date} balance {acct} {bal} GBP")
 
     def get_transactions(self, fr, to):
         url = f"/api/v2/feed/account/{self.uid}/settled-transactions-between"
@@ -72,7 +96,11 @@ class Account:
             "minTransactionTimestamp": f"{fr}T00:00:00.000Z",
             "maxTransactionTimestamp": f"{to}T00:00:00.000Z",
         }
-        r = httpx.get(self.conf.base + url, params=params, headers=self.headers)
+        r = httpx.get(
+            self.conf.base + url,
+            params=params,
+            headers=self.headers,
+        )
         data = r.json()
         return data
 
@@ -81,24 +109,38 @@ class Account:
         for it in tr["feedItems"]:
             if verbose:
                 log(it)
-            date = it["transactionTime"][:10]
-            payee = it.get("counterPartyName", "???")
-            ref = " ".join(it["reference"].split())
-            acct = f"Assets:Starling:{self.acc.capitalize()}"
-            cp = self.get_cp(it)
-            amt = it["amount"]["minorUnits"] / 100
-            amt = amt if it["direction"] == "IN" else -amt
-            print(f'{date} * "{payee}" "{ref}"')
-            print(f"  {acct} {amt} GBP")
-            print(f"  {cp}\n")
+            try:
+                date = it["transactionTime"][:10]
+                payee = it.get("counterPartyName", "???")
+                ref = " ".join(it["reference"].split())
+                acct = f"Assets:Starling:{self.acc.capitalize()}"
+                cp = self.get_cp(it)
+                amt = it["amount"]["minorUnits"] / 100
+                amt = amt if it["direction"] == "IN" else -amt
+                print(f'{date} * "{payee}" "{ref}"')
+                print(f"  {acct} {amt} GBP")
+                print(f"  {cp}\n")
+            except KeyError as e:
+                print(f"KeyError on {e}", sys.stderr)
+                pprint(it, stream=sys.sdterr)
 
 
-def main(acc: str, fr: str, to: str, balance: bool = False, verbose: bool = False):
-    account = Account(acc)
-    if balance:
-        print(account.balance)
-    else:
-        account.print_transactions(fr, to, verbose=verbose)
+def main(
+    accs: str,
+    fr: str = typer.Option(None),
+    to: str = typer.Option(None),
+    balance: bool = False,
+    verbose: bool = False,
+):
+    accs = parse_accs(accs)
+    for acc in accs:
+        account = Account(acc)
+        if balance:
+            account.print_balance(verbose=verbose)
+        else:
+            print(f"* {acc} - {to}")
+            account.print_transactions(fr, to, verbose=verbose)
+            print("\n\n\n")
 
 
 if __name__ == "__main__":
