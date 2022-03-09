c559d6c47c12549e48b4fc888d6fb3b7a1130f2c
Chris Arderne <chris@rdrn.me>
2022-03-09 10:41:25 +0000

    Simplify tokens, acc names

--- star.py
+++ star.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python3
 
-import sys
-from pprint import pprint
-from pathlib import Path
 import datetime
+from pathlib import Path
+from pprint import pprint
+import sys
 from typing import Union
 
-import httpx
-import attr
-from attr import define
-import typer
-import yaml
 from beancount.core.amount import Amount
 from beancount.core.data import Transaction, Posting, Balance, Note, new_metadata
 from beancount.core.flags import FLAG_OKAY
 from beancount.ingest.extract import print_extracted_entries
 from beancount.utils.date_utils import parse_date_liberally
 from decimal import Decimal
+import httpx
+import typer
+import yaml
 
 token_path = Path(__file__).parent.resolve() / "tokens"
 config_path = Path(__file__).parents[0] / "config.yml"
@@ -34,36 +32,27 @@ def log(it):
     print()
 
 
-@define(init=False)
 class Config:
-    base: str
-    cps: dict
-    tokens: dict
-    joint: list
-    users: dict
-
     def __init__(self):
         with open(config_path) as f:
             config = yaml.safe_load(f)
         self.base = config["base"]
         self.cps = config["cps"]
-        self.tokens = {p.stem: open(p).read().strip() for p in token_path.glob("*")}
         self.joint = config["jointAccs"]
         self.users = config["userIds"]
 
 
-@define
 class Account:
-    acc: str
-    full_account: str
-    conf: Config
-    verbose: bool = False
-    uid: str = attr.ib(init=False)
-
-    def __attrs_post_init__(self):
+    def __init__(self, acc, verbose=False):
+        self.acc = acc
+        self.verbose = verbose
+        self.conf = Config()
+        self.account_name = ":".join((w.capitalize() for w in self.acc.split("_")))
+        self.token = open(token_path / self.acc).read().strip()
+        self.headers = {"Authorization": f"Bearer {self.token}"}
         self.uid = self.get_uid()
 
-    def get_uid(self):
+    def get_uid(self) -> str:
         url = "/api/v2/accounts"
         r = httpx.get(self.conf.base + url, headers=self.headers)
         data = r.json()
@@ -75,24 +64,6 @@ class Account:
         if self.verbose:
             log(f"{uid=}")
         return uid
-
-    def get_cp(self, it):
-        try:
-            return self.conf.cps[it["spendingCategory"]]
-        except KeyError:
-            return self.conf.cps["DEFAULT"]
-
-    @property
-    def token(self):
-        try:
-            return self.conf.tokens[self.acc]
-        except KeyError:
-            echo("Token not found, make sure it exists under tokens/")
-            sys.exit()
-
-    @property
-    def headers(self):
-        return {"Authorization": f"Bearer {self.token}"}
 
     def get_balance_data(self) -> Decimal:
         url = f"/api/v2/accounts/{self.uid}/balance"
@@ -109,14 +80,14 @@ class Account:
         meta = new_metadata("starling-api", 0)
         tomorrow = datetime.date.today() + datetime.timedelta(days=1)
 
-        balance = Balance(meta, tomorrow, self.full_account, amt, None, None)
+        balance = Balance(meta, tomorrow, self.account_name, amt, None, None)
         if display:
             print_extracted_entries([balance], file=sys.stdout)
 
-        note = Note(meta, tomorrow, self.full_account, "bean-extract")
+        note = Note(meta, tomorrow, self.account_name, "bean-extract")
         return [balance, note]
 
-    def get_transaction_data(self, fr: str) -> list[dict]:
+    def get_transaction_data(self, since: str) -> list[dict]:
         # get default category UID
         url = "/api/v2/accounts"
         r = httpx.get(self.conf.base + url, headers=self.headers)
@@ -141,7 +112,7 @@ class Account:
         for category in spaces_categories + [default_category]:
             url = f"/api/v2/feed/account/{self.uid}/category/{category}"
             params = {
-                "changesSince": f"{fr}T00:00:00.000Z",
+                "changesSince": f"{since}T00:00:00.000Z",
             }
             r = httpx.get(
                 self.conf.base + url,
@@ -152,8 +123,8 @@ class Account:
             all_data.extend(data["feedItems"])
         return sorted(all_data, key=lambda x: x["transactionTime"])
 
-    def transactions(self, fr: str, display: bool = False) -> list[Transaction]:
-        tr = self.get_transaction_data(fr)
+    def transactions(self, since: str, display: bool = False) -> list[Transaction]:
+        tr = self.get_transaction_data(since)
         txns = []
         for i, item in enumerate(tr):
             if self.verbose:
@@ -177,11 +148,15 @@ class Account:
                 # must add this to not get unwanted UIDs returned
                 user = None
 
+            try:
+                cp = self.conf.cps[item["spendingCategory"]]
+            except KeyError:
+                cp = self.conf.cps["DEFAULT"]
+
             extra_meta = {"user": user} if user else None
             meta = new_metadata("starling-api", i, extra_meta)
-
-            p1 = Posting(self.full_account, Amount(amt, "GBP"), None, None, None, None)
-            p2 = Posting(self.get_cp(item), None, None, None, None, None)
+            p1 = Posting(self.account_name, Amount(amt, "GBP"), None, None, None, None)
+            p2 = Posting(cp, None, None, None, None, None)
             txn = Transaction(
                 meta=meta,
                 date=date,
@@ -200,31 +175,25 @@ class Account:
         return txns
 
 
-def extract(acc: str, full_account: str, fr: str) -> list[Union[Transaction, Balance]]:
-    conf = Config()
-    account = Account(acc, full_account, conf)
-    transactions = account.transactions(fr)
+def extract(acc: str, since: str) -> list[Union[Transaction, Balance]]:
+    """bean-extract entrypoint"""
+    account = Account(acc)
+    transactions = account.transactions(since)
     balances = account.balances()
     return transactions + balances
 
 
-def main(
-    acc: str,
-    fr: str = None,
-    balance: bool = False,
-    verbose: bool = False,
-):
-    if not fr and not balance:
-        echo("Need to provide a from date for transactions")
+def main(acc: str, since: str = None, balance: bool = False, verbose: bool = False):
+    """CLI entrypoint"""
+    if not since and not balance:
+        echo("Need to provide a 'since' date for transactions")
         return
 
-    conf = Config()
-
-    account = Account(acc, f"Assets:Starling:{acc.capitalize()}", conf, verbose)
+    account = Account(acc, verbose)
     if balance:
         account.balances(display=True)
     else:
-        account.transactions(fr, display=True)
+        account.transactions(since, display=True)
 
 
 if __name__ == "__main__":
